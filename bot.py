75ca0d6ed7da7e74bcd0b357c7d946207f37ea77
harattonycave <ilkercakmakci@yahoo.com>
2026-05-24 19:02:58 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_CHAT_ID = "123456789"
+
+async def forward_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.forward_message(
+        chat_id=ADMIN_CHAT_ID,
+        from_chat_id=update.effective_chat.id,
+        message_id=update.message.message_id
+    )
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(MessageHandler(filters.ALL, forward_message))
+
+print("Bot çalışıyor...")
+
+app.run_polling()
